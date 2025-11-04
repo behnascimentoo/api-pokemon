6964712a032edb7de144854d4efb18f293118ec0
behnascimentoo <brendantrabalhos@gmail.com>
2025-11-04 15:01:24 -0300

    primeiro commit

---
+++ src/main/java/com/pokemon/api_pokemon/ApiPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.api_pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokemonApplication.class, args);
+	}
+
+}
